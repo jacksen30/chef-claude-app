d8402c140144dcbe0bc7a3e9d7e5f51eaccf1a2f
Jacksen Nillson <nillsonjacksen@gmail.com>
2025-01-22 17:29:07 +0800

    update get-recipe-container to conditionally render only when the ingredients list contains more than 3 items

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -27,13 +27,14 @@ export default function Main() {
             <section>
                 <h2>Ingredients on hand:</h2>
                 <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+                {ingredients.length > 3 ?
                 <div className="get-recipe-container">
                     <div>
                         <h3>Ready for a recipe?</h3>
                         <p>Generate a recipe from your list of ingredients.</p>
                     </div>
                     <button>Get a recipe</button>
-                </div>
+                </div> : null }
             </section> : null }
         </main>
     )
