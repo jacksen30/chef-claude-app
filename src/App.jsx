80374e7fd59e2dc39ea661acbe3e8a7ef8aab774
Jacksen Nillson <nillsonjacksen@gmail.com>
2025-02-26 18:46:51 +0800

    deleted app.css styling file

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { react, useState } from 'react'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 
-import './App.css'
-
 function App() {
   return (
     <>
